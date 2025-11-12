796a4ad5673985897bf7b758caa9c4b372ace0d2
nrocha02 <nico.rocha@duocuc.cl>
2025-11-12 15:48:26 -0300

    Jobs eliminados

--- Fase 2/Evidencias Proyecto/Web/apps/run_daily_stock_job.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Users\Caro\Documents\2025_MA_CAPSTONE_704D_GRUPO_2\Fase 2\Evidencias Proyecto\Web"
-"C:\Users\Caro\Documents\2025_MA_CAPSTONE_704D_GRUPO_2\Fase 2\Evidencias Proyecto\Web\.venv\Scripts\python.exe" manage.py daily_stock_job --admin-email=cordillerapetschile@gmail.com
